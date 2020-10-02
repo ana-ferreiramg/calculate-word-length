69e35668aad097e5ac1575efecddbdc4bb1e82ac
Ana Ferreira <60697599+ana-ferreiramg@users.noreply.github.com>
2020-10-02 18:04:25 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.getElementById('btn').addEventListener("click", function () {
     var word = document.getElementById('word').value.length;
 
-    document.getElementById('result').innerHTML = "Lenght is: " + word;
+    document.getElementById('result').innerHTML = "Length is: " + word;
 });
